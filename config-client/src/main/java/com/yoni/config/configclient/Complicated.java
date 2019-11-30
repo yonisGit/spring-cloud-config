e5b054be674678749b92da76033d106ad9d0cd8b
yonisGit <yoniel@post.bgu.ac.il>
2019-11-30 16:33:43 +0200

    Add files via upload

---
+++ config-client/src/main/java/com/yoni/config/configclient/Complicated.java
@@ -0,0 +1,9 @@
+package com.yoni.config.configclient;
+
+public class Complicated {
+//    MongoDbFactory mongoDbFactory;
+//
+//    public void createConnection(){
+//        mongoDbFactory.getDb();
+//    }
+}
